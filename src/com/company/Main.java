58e0b8ad8acdb90ee8ba92d0b152de06efa303c9
Sofia1381 <sofia.ghayur@gamil.com>
2022-05-17 23:53:05 -0400

    Solar System project for khadi basics

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        SolarSystem XFact=new SolarSystem();
+        XFact.Sun="1";
+        XFact.Moon="4";
+        XFact.Planet="2";
+        XFact.Stars="10000";
+    }
+}
